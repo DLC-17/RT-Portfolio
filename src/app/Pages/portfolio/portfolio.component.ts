2816f43ea10063794465c051fc56bf4be8269b3c
D C <84093031+DLC-17@users.noreply.github.com>
2024-06-28 11:00:31 -0700

    Added various images for the portfolio updated footer with github logo

--- src/app/Pages/portfolio/portfolio.component.ts
+++ src/app/Pages/portfolio/portfolio.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
