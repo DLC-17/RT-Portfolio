bb7ffc45da2374da29d585c167ec2c36135520db
D C <84093031+DLC-17@users.noreply.github.com>
2024-07-10 13:18:51 -0700

    Reposistioned files in assets forlder an removed items on home page for redesign

---
+++ assets/images/Carousel-images.ts
@@ -0,0 +1,5 @@
+export const Photos=[
+    {image: '/assets/images/MoA/dc.JPG',id: '0',Subtitle: 'somehting', title: 'number 1'},
+    {image: '/assets/images/MoA/GroupShot.jpg',id:'1',Subtitle: 'somehting', title: 'number 2'},
+    {image: '/assets/images/MoA/Jm.JPG',id:'2',Subtitle: 'somehting', title: 'number 3'},
+  ]
