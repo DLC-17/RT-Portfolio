6083e939941aaa5ed57558c4f5150d2ca2bbfb23
D C <84093031+DLC-17@users.noreply.github.com>
2024-07-31 18:07:32 -0700

    implemented lazy loading for images

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PortfolioComponent } from './Pages/portfolio/portfolio.component';
-import { AppComponent } from './Pages/Main/app.component';
 import { ContactComponent } from './Pages/contact/contact.component';
 import { HomeComponent } from './Pages/home/home.component';
 export const routes: Routes = [
   { path: 'portfolio', component: PortfolioComponent },
   {path: 'contact', component:ContactComponent}
-  ,{path:'home', component:HomeComponent}
+  ,{path:'home', component:HomeComponent, pathMatch:'full'}
   ,{ path: '**', component:HomeComponent, pathMatch:'full'},
   
   
